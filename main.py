14d9d758dd6fc617310b068ab44cbe0c5c73f9eb
Dylan Scoggins <dylanscoggins@gmail.com>
2023-04-11 16:33:46 -0500

    Minor update to main.py

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ import fileio
 import graph
 
 # ? should main.py handle file io or graph.py?
-# fileio.open_file_CSV("531 4-day program - Leader #1 (5s PRO_FSL).csv", "r")
+fileio.open_file_CSV("531 4-day program - Leader #1 (5s PRO_FSL).csv")
 
 # * Generates graphs from data provided to generate_graph function in graph.py
-graph.generate_graph("Leader 1 Cycle Data",[1,2,3],[2,4,1])
-graph.generate_graph("Leader 2 Cylce Data",[1,2,3],[2,6,3])
-graph.generate_graph("Anchor Cycle Data",[1,2,3],[2,6,3])
+# graph.generate_graph("Leader 1 Cycle Data",[1,2,3],[2,4,1])
+# graph.generate_graph("Leader 2 Cylce Data",[1,2,3],[2,6,3])
+# graph.generate_graph("Anchor Cycle Data",[1,2,3],[2,6,3])
