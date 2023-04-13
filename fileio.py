e98ec1bd941060e66e659e44bfb9a2756f7bd2ec
Dylan Scoggins <dylanscoggins@gmail.com>
2023-04-13 09:43:43 -0500

    Changed packages from csv to pandas

--- fileio.py
+++ fileio.py
@@ -3,13 +3,9 @@
 
 # TODO: convert data from CSV to JSON format at some point
 #  import json
-import csv
+import pandas
 
 def open_file_CSV(fname):
-    with open(fname, newline='', encoding="utf-8") as csvfile:
-        reader = csv.reader(csvfile)
-        for row in reader:
-            print(row)
-            for col in reader:
-                print(col)
-
+    with open(fname, newline='', encoding='utf-8') as csvfile:
+        dframe = pandas.read_csv(csvfile)
+        print(dframe)
