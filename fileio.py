0a9ec247aa74bf52af88f2cfae225dcac8abb033
Dylan Scoggins <dylanscoggins@gmail.com>
2023-04-13 21:28:54 -0500

    Update to variables/module names for clarity

--- fileio.py
+++ fileio.py
@@ -1,11 +1,11 @@
 
-# * Module for handling file manipulation
+# * Module for handling file manipulation and data processing
 
 # TODO: convert data from CSV to JSON format at some point
 #  import json
-import pandas
+import pandas as pd
 
 def open_file_CSV(fname):
     with open(fname, newline='', encoding='utf-8') as csvfile:
-        dframe = pandas.read_csv(csvfile)
-        print(dframe)
+        exercise_dframe = pd.read_csv(csvfile, index_col='Week')
+        print(exercise_dframe)
