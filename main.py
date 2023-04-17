1ddc92ee7bd0c48e105caf0e88504ed9f7f77eb5
Dylan Scoggins <dylanscoggins@gmail.com>
2023-04-17 16:25:02 -0500

    Minor updates/cleanup

--- main.py
+++ main.py
@@ -10,11 +10,14 @@ def main():
 
     # ? should main.py handle file io or graph.py?
     # fileio.open_file_CSV("531 4-day program - Copy of Leader #1 (5s PRO_FSL).csv")
-    fileio.print_data(fileio.process_file_CSV("531 4-day program - TM.csv"))
+    fileio.print_data(fileio.process_file_CSV('531 4-day program - TM.csv'))
 
     # * Generates graphs from data provided to generate_graph function in graph.py
     # graph.generate_graph("Leader 1 Cycle Data",[1,2,3],[2,4,1])
     # graph.generate_graph("Leader 2 Cylce Data",[1,2,3],[2,6,3])
     # graph.generate_graph("Anchor Cycle Data",[1,2,3],[2,6,3])
+
+    # * Write data to makeshift database aka CSV file
+    # fileio.write_to_CSV(fileio.process_file_CSV('531 4-day program - TM.csv'), '531 Log.csv')
 
 main()
