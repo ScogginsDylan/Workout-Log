5f63d4a1d8056a9b1a5f6c1c9014fe1fdc70c0b6
Dylan Scoggins <dylanscoggins@gmail.com>
2023-04-19 12:04:02 -0500

    filio.py now simply returns numpy array

--- fileio.py
+++ fileio.py
@@ -14,17 +14,15 @@ def process_file_CSV(fname):
         exercise_dframe = pd.read_csv(csvfile)
         exercise_nparray = np.array(exercise_dframe)
 
-        indices = [0,1]
-        processed_nparray = np.take(exercise_nparray, indices, axis=1)
-        return processed_nparray
+        return exercise_nparray
 
 # * Converts np array back to pandas df and writes data to makeshift database aka CSV file
 # TODO: 1) determine structure of database
 # TODO: 2) account for existing file to append to
-def write_to_CSV(nparray, fname):
-    new_df = pd.DataFrame(nparray, columns=['Exercise','Weight'])
-    new_df.to_csv(fname, sep='\t', encoding='utf-8', index=False)
-    return
+# def write_to_CSV(nparray, fname):
+#     new_df = pd.DataFrame(nparray, columns=['Exercise','Weight'])
+#     new_df.to_csv(fname, sep='\t', encoding='utf-8', index=False)
+#     return
 
 # * For testing purposes only
 def print_data(nparray):
