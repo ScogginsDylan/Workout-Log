419f215086980bfd20ca36149b3b76324f6bac3d
Dylan Scoggins <dylanscoggins@gmail.com>
2023-04-19 12:04:51 -0500

    Now slices np array and reshapes for graphing

--- graph.py
+++ graph.py
@@ -1,13 +1,22 @@
 
 # * Handles graph rendering
 
-import fileio
+import numpy as np
 import matplotlib.pyplot as plt
 
-# * Generate graph from Leader#1 data
-def generate_graph(title, x_axis, y_axis):
+# * Generate graph from provided nparray data
+def generate_graph(title, nparray):
 
-    plt.plot(x_axis,y_axis)
+    x_indices = [0]
+    y_indices = [1]
+    x_axis = np.take(nparray, x_indices, axis=1)
+    y_axis = np.take(nparray, y_indices, axis=1)
+
+    # * Converts axis to shape that matplotlib likes
+    mod_x_axis = x_axis.ravel()
+    mod_y_axis = y_axis.ravel()
+
+    plt.plot(mod_x_axis,mod_y_axis)
 
     plt.xlabel('Time')
     plt.ylabel('Weight')
