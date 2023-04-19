7c8692c9c10b4abe45220ca7ac3a1cd0588a83d7
Dylan Scoggins <dylanscoggins@gmail.com>
2023-04-19 12:39:58 -0500

    Minor cleanup, TODO: implement write_to_CSV

--- main.py
+++ main.py
@@ -1,23 +1,18 @@
 # Start of a new workout log app to track progress using graphical data.
-# Uses TKinter, Pandas, MatPlotLib, and Numpy modules to display data over period of time.
+# Uses Pandas, MatPlotLib, and Numpy modules to display graphical data over period of time.
 
-# * main.py is the driver file for the app
+# * main.py is the driver file for the program
 
 import fileio
 import graph
 
 def main():
 
-    # ? should main.py handle file io or graph.py?
-    # fileio.open_file_CSV("531 4-day program - Copy of Leader #1 (5s PRO_FSL).csv")
-    # fileio.print_data(fileio.process_file_CSV('531 4-day program - TM Log.csv'))
-
     # * Generates graphs from data provided to generate_graph function in graph.py
-    graph.generate_graph("Bench Press",fileio.process_file_CSV('531 4-day program - TM Log.csv'))
-    # graph.generate_graph("Leader 2 Cylce Data",[1,2,3],[2,6,3])
-    # graph.generate_graph("Anchor Cycle Data",[1,2,3],[2,6,3])
+    graph.generate_graph('531 TM Progress',fileio.process_file_CSV('531 4-day program - TM Log.csv'))
 
     # * Write data to makeshift database aka CSV file
+    # TODO: finish write_to_CSV function
     # fileio.write_to_CSV(fileio.process_file_CSV('531 4-day program - TM.csv'), '531 Log.csv')
 
 main()
